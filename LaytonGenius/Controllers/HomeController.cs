f9ae1b498a04bb9065211772589b0f54f5f5e59d
Scott Olson <scottolsonofficial@gmail.com>
2022-03-23 21:47:22 -0600

    fixes to edit not working

--- LaytonGenius/Controllers/HomeController.cs
+++ LaytonGenius/Controllers/HomeController.cs
@@ -81,18 +81,18 @@ namespace LaytonGenius.Controllers
         }
         
         [HttpGet]
-        public IActionResult Edit(int a)
+        public IActionResult Edit(int appid)
         {
             ViewBag.Responses = _appContext.Appointments.ToList();
-            var application = _appContext.Appointments.Single(x => x.AppId == a);
+            var application = _appContext.Appointments.Single(x => x.AppId == appid);
             return View("Create", application);
         }
 
 
         [HttpPost]
-        public IActionResult Edit(Appointment a)
+        public IActionResult Edit(Appointment app)
         {
-            _appContext.Update(a);
+            _appContext.Update(app);
             _appContext.SaveChanges();
             return RedirectToAction("AppointmentsView");
         }
